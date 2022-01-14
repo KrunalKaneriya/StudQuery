40fd0d1ef0ba5104a16cff770b50b20989b9841e
Krunal Kaneria <prokillerkk@outlook.com>
2022-01-14 21:45:01 +0530

    Created Password Middleware File

---
+++ middlewares/password.js
@@ -0,0 +1,13 @@
+const crypt = require("bcrypt");
+
+const hashPassword = async (pw) => {
+    const hash = await crypt.hash(pw,10);
+    return hash;
+}
+
+const comparePassword = async (pw,hash) => {
+    const result = await crypt.compare(pw,hash);
+    return result;
+}
+
+module.exports = {hashPassword,comparePassword};
