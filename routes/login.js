056d0f4e3361bd96399b6ab2c85fc855cfb00af6
Krunal Kaneria <prokillerkk@outlook.com>
2022-01-14 21:42:21 +0530

    Added Login With Hashing And Updating last Login Time

---
+++ routes/login.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const session = require("express-session");
+const crypt = require("bcrypt");
+
+
+
+router.get("/login",(req,res) => {
+    res.render("login");
+})
+
+router.post("/login",async (req,res) => {
+    const {username,password} = req.body; //Fetching Username And Password From Browser
+
+    const user = await User.findOne({username}); //Finding The Username in DB
+
+    if(user) {
+        const result = await crypt.compare(password,user.userHash);
+
+        if(result) {
+            res.send("Welcome Home")
+        } else {
+            res.send("Username Or Password is Incorrect");
+        }
+
+        const {_id : id} = user;
+
+        //Find The Logged in User and run Update Query to Update the lastLoggedIn To Latest Time
+        await User.findByIdAndUpdate(id , {$set: {"lastLoggedIn" : new Date() }});
+
+        
+
+    } else {
+        res.send("User is not Registered");
+    }
+    
+    
+    
+})
+
+module.exports = router;
+
+
+/*
+1.To Login Using Username and Password
+2.To check that the user is there 
+3.and if the user is there then get the userhash and check the password and if it is valid then allow the user to login
+4.Otherwise send that the user is not found
+
+
+*/
