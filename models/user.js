1e31e0a5e9f24dc493c3c01e3759992017f8b2aa
Krunal Kaneria <prokillerkk@outlook.com>
2022-01-14 21:40:32 +0530

    Updated Database and added Timestamp

--- models/user.js
+++ models/user.js
@@ -2,28 +2,41 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    userName:{
+    username:{
         type:String,
         required:true,
         default:"User Name is Not Given"
     },
+    userHash:{
+        type:String,
+        required:[true,"Userhash is Required"]
+    },
+    password:{
+        type:String
+    },
     alias:String,
-    userEmail:{
+    email:{
         type:String,
         required:true,
         default:"Email Address Not Specified"
     },
-    userAge:Number,
+    age:Number,
     description:String,
-    userJoiningDate:{
-        type:Date,
-        required:true,
-        default:"User Joining Date Not Given"
-    },
-    lastAccessTime:Date,
-    totalQuestions:Number,
-    totalAnswers:Number,
-    studyingIn:String
-})
+    studyingIn:String,
+    questions:[{
+        type:Schema.Types.ObjectId,
+        ref:"Question"
+    }],
+    answers:[{
+        type:Schema.Types.ObjectId,
+        ref:"Answer"
+    }],
+    city:String,
+    lastLoggedIn:{
+        type:Date
+    }
+},{timestamps:true});
+
+
 
 module.exports = mongoose.model("User",UserSchema);
