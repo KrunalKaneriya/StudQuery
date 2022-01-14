975a719db9fbafe4f06587e61ce343d216638f99
Krunal Kaneria <prokillerkk@outlook.com>
2022-01-14 21:43:01 +0530

    Created Question Router

---
+++ routes/question.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const Question = require("../models/question");
+const session = require("express-session");
+
+router.get("/addQuestion",async (req,res) => {
+
+})
