35950d6db2aacc84d829f96cefefd9bcae29b11e
Krunal Kaneria <prokillerkk@outlook.com>
2022-01-14 20:56:31 +0530

    Added Home Signup And Login Router

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
 const express = require('express');
 const mongoose = require("mongoose");
-
+const path = require("path");
 const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000,() => {
+/*********************
+ * IMPORTING ROUTERS *
+ *********************/
+
+const login = require("./routes/login");
+const signup = require("./routes/signup");
+const home = require("./routes/home");
+
+/*************************************
+ * CONNECTING TO DATABASE AND SERVER *
+ *************************************/
+app.listen(3000, () => {
     console.log('Started Server At Port 3000');
 })
+
+mongoose.connect('mongodb://localhost:27017/studquery')
+    .then(() => {
+        console.log("Mongodb Connected");
+    })
+    .catch(e => {
+        console.log(e);
+        console.log("Error Connecting to Mongodb");
+    })
+
+
+/**********************
+ * USING LOGIN ROUTER *
+ **********************/
+
+    app.use(login);
+
+/***********************
+ * USING SIGNUP ROUTER *
+ ***********************/
+
+    app.use(signup);
+
+
+/*********************
+ * USING HOME ROUTER *
+ *********************/
+
+    app.use(home);
