5dc5740b99b49191dfb21d9c62a938ebc36e1b28
Krunal Kaneria <prokillerkk@outlook.com>
2022-01-14 23:45:44 +0530

    Added Question Router and created Get And Post of ques

--- routes/question.js
+++ routes/question.js
@@ -4,6 +4,30 @@ const User = require("../models/user");
 const Question = require("../models/question");
 const session = require("express-session");
 
-router.get("/addQuestion",async (req,res) => {
+router.get("/user/:id/addQuestion",async (req,res) => {
+    const {id} = req.params;
+    const user = await User.findById(id);
+    res.render("question",{user});
+})
+
+router.post("/user/:id/addQuestion",async (req,res) => {
+    const {id} = req.params;
+    const user = await User.findById(id);
+    const {questionTitle,questionDescription} = req.body;
+
+    const question = new Question({
+        questionTitle,
+        questionDescription
+    })
+
+    user.questions.push(question);
+    question.user = user;
+
+    await question.save();
+    await user.save();
+
+    res.redirect(`/user/${user._id}`);
 
 })
+
+module.exports = router;
