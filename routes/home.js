38c5dc7618e4781799a1a85d78b52ed20f30ca0f
Krunal Kaneria <prokillerkk@outlook.com>
2022-01-16 00:03:31 +0530

    Now Sending Questions As Array in Home Route

--- routes/home.js
+++ routes/home.js
@@ -3,17 +3,35 @@ const router = express.Router();
 const User = require("../models/user");
 const session = require("express-session");
 const crypt = require("bcrypt");
+const Question = require("../models/question");
 
 
 
-router.get("/",async (req,res) => {
+router.get("/", async (req, res) => {
 
-    const {username,userid,email} = req.session;
+    const { username, userid, email } = req.session;
 
 
-    const users = await User.find({});
+    // const users = await User.find({});
+    // const usersCount = users.length;
 
-    res.render("home",{users,username,userid,email});
+    const questions = await Question.find({}).populate('user').sort("asc");
+
+
+
+    // for (let singleQuestion of question) {
+    //     const { questionTitle, questionDescription } = singleQuestion;
+    //     const { username, alias, email, city } = singleQuestion.user;
+
+    //     // console.log(`The Name is ${username} and Their Title is ${questionTitle} and the Description
+    //     //         is ${questionDescription}`);
+    //     console.log(singleQuestion);
+    // }
+
+    res.render("home",{questions,username,userid,email});
+
+
+    // res.send(user.questions)
 })
 
 module.exports = router;
