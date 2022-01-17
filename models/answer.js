c5d827a8eb3663c998d310946a04e602f9a334ce
Krunal Kaneria <prokillerkk@outlook.com>
2022-01-17 07:17:45 +0530

    Added Question Model Reference

--- models/answer.js
+++ models/answer.js
@@ -12,6 +12,10 @@ const AnswerSchema = new Schema({
     user:{
         type:Schema.Types.ObjectId,
         ref:'User'
+    },
+    question:{
+        type:Schema.Types.ObjectId,
+        ref:"Question"
     }
 },{timestamps:true})
 
