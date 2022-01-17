a97939ebd962cd24bd47839df2f338884aaf4a33
Krunal Kaneria <prokillerkk@outlook.com>
2022-01-17 07:19:17 +0530

    Javascript for Utilities

---
+++ public/js/homePage.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    // $(".answer-container").hide();
+
+    $(".reply-btn").on('click',function (e) {
+        
+        $(this).click(function() {
+            // $(".answer-container").slideToggle();
+        })
+        
+    })
+})
+
