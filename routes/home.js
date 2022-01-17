41f441672cd61738dcf8d59081170936934952eb
Krunal Kaneria <prokillerkk@outlook.com>
2022-01-17 07:20:59 +0530

    Added Some Extra Code in Home Router

--- routes/home.js
+++ routes/home.js
@@ -4,34 +4,74 @@ const User = require("../models/user");
 const session = require("express-session");
 const crypt = require("bcrypt");
 const Question = require("../models/question");
-
+const Post = require("../models/postSchema");
 
 
 router.get("/", async (req, res) => {
 
     const { username, userid, email } = req.session;
 
+    // res.send("The User is Logged In...");
+    
 
-    // const users = await User.find({});
+
+    // const posts = await Post.find().populate("user").populate("question");
+    // const users = await User.find({}).populate("questions");
+
     // const usersCount = users.length;
 
     const questions = await Question.find({}).populate('user').sort("asc");
+    // const users = await User.find().lean();
+    // const questions = await Questions.find()
 
+    // printUserAndQuestion(users);
+    // console.log(questions)
+
+    // console.log(posts);
 
 
     // for (let singleQuestion of question) {
     //     const { questionTitle, questionDescription } = singleQuestion;
     //     const { username, alias, email, city } = singleQuestion.user;
 
-    //     // console.log(`The Name is ${username} and Their Title is ${questionTitle} and the Description
-    //     //         is ${questionDescription}`);
+        // console.log(`The Name is ${username} and Their Title is ${questionTitle} and the Description
+        //         is ${questionDescription}`);
     //     console.log(singleQuestion);
     // }
 
-    res.render("home",{questions,username,userid,email});
+
+
+    if(req.session.username) {
+    res.render("home",{username,questions});
+        
+    } else {
+        res.render("home",{questions});
+
+    }
+    // console.log(questions);
 
 
     // res.send(user.questions)
 })
+
+    const printUserAndQuestion = async (users) => {
+        let newUser = [];
+        let question = [];
+        for(let user of users) {
+            if(user.username === "Krunal Kaneria") {
+                for(let singleQuestion of user.questions) {
+                    newUser.push(user);
+                    question.push(singleQuestion);
+                }
+
+            }
+        }
+
+        // console.log(newUser);
+        for(let user of newUser) {
+            console.log(user);
+        }
+    }
+
 
 module.exports = router;
