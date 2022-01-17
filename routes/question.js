5d4d330f6d6399cff8a34a86e4ee3778d59f880b
Krunal Kaneria <prokillerkk@outlook.com>
2022-01-17 07:21:42 +0530

    Added Post Schema Reference

--- routes/question.js
+++ routes/question.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user");
 const Question = require("../models/question");
 const session = require("express-session");
+const Post = require("../models/postSchema");
 
 router.get("/user/:id/addQuestion",async (req,res) => {
     const {id} = req.params;
@@ -25,6 +26,13 @@ router.post("/user/:id/addQuestion",async (req,res) => {
 
     await question.save();
     await user.save();
+
+    const post = new Post({
+        user,
+        question
+    })
+
+    await post.save();
 
     res.redirect(`/user/${user._id}`);
 
