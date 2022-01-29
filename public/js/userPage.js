fe0939cd60844fe00006c05e3e7783dfe0357e33
Krunal Kaneria <prokillerkk@outlook.com>
2022-01-29 11:38:30 +0530

    Added Functionality in User Page

---
+++ public/js/userPage.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $("#edit-button-wrapper").click(function() {
+        $("#edit-button").fadeOut("slow");
+        $("#delete-button-wrapper").fadeOut("slow");
+        $("#username").prop("disabled",false);
+        $("#alias").prop("disabled",false);
+        $("#email").prop("disabled",false);
+        $("#age").prop("disabled",false);
+        $("#city").prop("disabled",false);
+        $("#studyingin").prop("disabled",false);
+        $("#description").prop("disabled",false);
+    })
+
+
+})
+
+let modal = document.getElementById("my-modal");
+let openBtn = document.getElementById("open-btn");
+let deleteBtn = document.getElementById("delete-btn");
+let cancelBtn = document.getElementById("cancel-btn");
+
+openBtn.addEventListener("click",function () {
+    modal.classList.remove("hidden");
+})
+
+cancelBtn.addEventListener("click",function () {
+    modal.classList.add("hidden");
+})
