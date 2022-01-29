9919f4b7f3047c386e59e3543453fc09d402bae1
Krunal Kaneria <prokillerkk@outlook.com>
2022-01-29 11:42:23 +0530

    Changed To Only Display All questions with users

--- routes/home.js
+++ routes/home.js
@@ -4,74 +4,19 @@ const User = require("../models/user");
 const session = require("express-session");
 const crypt = require("bcrypt");
 const Question = require("../models/question");
-const Post = require("../models/postSchema");
-
-
-router.get("/", async (req, res) => {
-
-    const { username, userid, email } = req.session;
-
-    // res.send("The User is Logged In...");
-    
-
-
-    // const posts = await Post.find().populate("user").populate("question");
-    // const users = await User.find({}).populate("questions");
-
-    // const usersCount = users.length;
-
-    const questions = await Question.find({}).populate('user').sort("asc");
-    // const users = await User.find().lean();
-    // const questions = await Questions.find()
-
-    // printUserAndQuestion(users);
-    // console.log(questions)
-
-    // console.log(posts);
-
-
-    // for (let singleQuestion of question) {
-    //     const { questionTitle, questionDescription } = singleQuestion;
-    //     const { username, alias, email, city } = singleQuestion.user;
-
-        // console.log(`The Name is ${username} and Their Title is ${questionTitle} and the Description
-        //         is ${questionDescription}`);
-    //     console.log(singleQuestion);
-    // }
 
 
 
-    if(req.session.username) {
-    res.render("home",{username,questions});
-        
-    } else {
-        res.render("home",{questions});
+//This router page is the home route and it will display the questions and answers of the users
+//This route will be displayed first when the user logs in.
 
-    }
-    // console.log(questions);
+router.get("/",async (req,res) => {
+    const posts = await Question.find().populate("answers").populate("user");
+    const userSession = req.session;
+    res.render("home",{posts,userSession});
+});
 
 
-    // res.send(user.questions)
-})
-
-    const printUserAndQuestion = async (users) => {
-        let newUser = [];
-        let question = [];
-        for(let user of users) {
-            if(user.username === "Krunal Kaneria") {
-                for(let singleQuestion of user.questions) {
-                    newUser.push(user);
-                    question.push(singleQuestion);
-                }
-
-            }
-        }
-
-        // console.log(newUser);
-        for(let user of newUser) {
-            console.log(user);
-        }
-    }
 
 
 module.exports = router;
