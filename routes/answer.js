4fa84c50b4249b374ed645ba480445c5abd37c73
Krunal Kaneria <prokillerkk@outlook.com>
2022-02-24 23:33:31 +0530

    Edited Answer Route and Added Voting System

--- routes/answer.js
+++ routes/answer.js
@@ -2,72 +2,166 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const Question = require("../models/question");
-const session = require("express-session");
-const Post = require("../models/postSchema");
 const Answer = require("../models/answer");
+const catchAsync = require("../utils/catchAsync");
 
-router.post("/question/:questionId/answer",async (req,res) => {
-    const {questionId} = req.params;
-    const userSession = req.session;
-    const {isLoggedIn,userid} = userSession;
-    if(isLoggedIn) {
+/************************************************
+ * ROUTE WHICH WILL ADD ANSWER TO A QUESTION. *
+ ************************************************/
+router.post("/question/:questionId/answer",catchAsync(async (req, res) => {
+		const { questionId } = req.params;
+		const userSession = req.session;
+		const { isLoggedIn, userid } = userSession;
+		if (isLoggedIn) {
+			const question = await Question.findById(questionId);
+			const user = await User.findById(userid);
+			const { answerDescription } = req.body;
+			const answer = new Answer({
+				answerDescription,
+			});
+			answer.question = question;
+			answer.user = user;
+			question.answers.push(answer);
+			user.answers.push(answer);
 
-        const question = await Question.findById(questionId);
+			answer.save();
+			question.save();
+			user.save();
 
-        const user =await User.findById(userid);
+			req.flash("success", "Your Answer is Added Successfully...");
+			res.redirect(`/question/${questionId}`);
+		} else {
+			req.flash("error", "You are Not Logged In. Cannot Post Answer");
+			res.redirect(`/question/${questionId}`);
+		}
+	})
+);
 
-        const {answerDescription} = req.body;
-        const answer = new Answer({
-            answerDescription
-        });
+/*************************************************
+ * ROUTE WHICH WILL REDIRECT TO ANSWER EDIT PAGE *
+ *************************************************/
+router.get("/question/:questionId/answer/:answerid",catchAsync(async (req, res) => {
+		const { answerid } = req.params;
+		const userSession = req.session;
+		const answer = await Answer.findById(answerid).populate("question").populate("user");
+		const question = answer.question;
+		res.render("editAnswer", { answer, question, userSession });
+	})
+);
 
-        answer.question = question;
-        answer.user = user;
-        question.answers.push(answer);
-        user.answers.push(answer);
+router.put("/question/:questionId/answer/:answerId/edit",catchAsync(async (req, res) => {
+		const { answerId } = req.params;
+		const answer = await Answer.findById(answerId).populate("user", "username").populate("question", "questionTitle questionDescription");
+		const question = answer.question;
+		await Answer.findByIdAndUpdate(answerId, { ...req.body });
+		req.flash("edit", "Answer is Edited...");
+		res.redirect(`/question/${answer.question._id}`);
+	})
+);
 
-        answer.save();
-        question.save();
-        user.save();
+router.put("/question/:questionId/answer/:answerId/voteinc",catchAsync(async (req, res) => {
+		const { questionId, answerId } = req.params;
+		const userSession = req.session;
+		const { userid, isLoggedIn } = userSession;
+		
+		if (!userSession.isLoggedIn) {
+			req.flash("error", "To vote you need to login first.");
+			const redirectUrl = req.session.redirectUrl || '/';
+			res.redirect(redirectUrl);
+		} else {
+			const answer = await Answer.findById(answerId);
 
-        res.redirect(`/question/${questionId}`);
+			const upVote = await Answer.exists({ _id: answerId, upVotes: { $in: userid } });
 
-    } else {
-        res.send("You are Not Logged In. Cannot Post Answer");
-    } 
+			if (!upVote) {
+				answer.upVotes.push(userid);
+				answer.votes += 1;
+			} else {
+				//Do Nothing
+			}
 
-})
+			const downVote = await Answer.exists({ _id: answerId, downVotes: { $in: userid } });
 
-router.get("/question/:questionId/answer/:answerid", async (req,res) => {
-    const {answerid} = req.params;
-    const userSession = req.session;
-    const answer = await Answer.findById(answerid).populate("question").populate("user");
-    const question = answer.question;
-    res.render("editAnswer",{answer,question,userSession});
-}) 
+			if (downVote) {
+				await Answer.findByIdAndUpdate(answerId, { $pull: { downVotes: userid } });
+				answer.votes += 1;
+			}
 
+			await answer.save();
 
-router.put("/question/:questionId/answer/:answerId/edit",async (req,res) => {
-    const {answerId} = req.params;
-    const answer = await Answer.findById(answerId).populate("user","username").populate("question","questionTitle questionDescription");
-    const question = answer.question;
-    await Answer.findByIdAndUpdate(answerId,{...req.body});
-    res.redirect(`/question/${answer.question._id}`);
-    //TODO Remaining Put Route in Answer Js And to Update the Answer Updated Time When Updating Answer
-})
+			// res.json({ votes: answer.votes });
 
-router.delete("/question/:questionId/answer/:answerId",async (req,res) => {
-    const userSession = req.session;
-    const {userid} = userSession;
-    const {answerId,questionId} = req.params;
+			// if(req.query.fullQuestion) {
 
-    const answer =await Answer.findById(answerId).populate("question");
+			    req.flash("success","You liked the question.");
+			    res.redirect(`/question/${questionId}`);
+			// } else {
+			//     req.flash("success","You liked the question.");
+			//     res.redirect("/");
+			// }
+		}
+	})
+);
 
-    await User.findByIdAndUpdate(userid,{ $pull:{answers:answerId} });
-    await Question.findByIdAndUpdate(questionId,{ $pull:{answers:answerId} });
-    const deletedAnswer = await Answer.findByIdAndDelete(answerId);
+router.put("/question/:questionId/answer/:answerId/votedec",catchAsync(async (req, res) => {
+		const { questionId, answerId } = req.params;
+		const userSession = req.session;
+		const { userid, isLoggedIn } = userSession;
 
-    res.redirect(`/question/${questionId}`);
-})
+		if (!userSession.isLoggedIn) {
+			req.flash("error", "To vote you need to login first.");
+			const redirectUrl = req.session.redirectUrl || '/';
+			res.redirect(redirectUrl);
+		} else {
+			const answer = await Answer.findById(answerId);
+			const downVote = await Answer.exists({ _id: answerId, downVotes: { $in: userid } });
+
+			if (!downVote) {
+				answer.downVotes.push(userid);
+				answer.votes -= 1;
+			} else {
+				// await Question.findByIdAndUpdate(questionId,{$pull:{downVotes : userid}});
+				// question.votes +=1;
+			}
+
+			const upVote = await Answer.exists({ _id: questionId, upVotes: { $in: userid } });
+
+			if (upVote) {
+				await Answer.findByIdAndUpdate(questionId, { $pull: { upVotes: userid } });
+				answer.votes -= 1;
+			}
+
+			await answer.save();
+			res.json({ votes: answer.votes });
+
+			// if(req.query.fullQuestion) {
+			    req.flash("success","You disliked the question.");
+			    res.redirect(`/question/${questionId}`);
+			// } else {
+			//     req.flash("success","You disliked the question.");
+			//     res.redirect("/");
+			// }
+		}
+
+		// const question = await Question.findByIdAndUpdate(questionId,{$inc: {votes:-1} },{new:true});
+		// await question.save();
+	})
+);
+
+router.delete("/question/:questionId/answer/:answerId",catchAsync(async (req, res) => {
+		const userSession = req.session;
+		const { userid } = userSession;
+		const { answerId, questionId } = req.params;
+
+		const answer = await Answer.findById(answerId).populate("question");
+
+		await User.findByIdAndUpdate(userid, { $pull: { answers: answerId } });
+		await Question.findByIdAndUpdate(questionId, { $pull: { answers: answerId } });
+		const deletedAnswer = await Answer.findByIdAndDelete(answerId);
+
+		req.flash("success", "Deleted Answer Successfully...");
+		res.redirect(`/question/${questionId}`);
+	})
+);
 
 module.exports = router;
