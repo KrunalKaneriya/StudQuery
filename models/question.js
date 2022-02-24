46063a82bb1066fc689af112c35145993f986ec8
Krunal Kaneria <prokillerkk@outlook.com>
2022-02-24 23:28:49 +0530

    Added StripHTML and Snippet as Data and Upvotes and Downvotes

--- models/question.js
+++ models/question.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const stripHTML = require("string-strip-html");
 const Schema = mongoose.Schema;
 
 const QuestionSchema = new Schema({
@@ -7,12 +8,30 @@ const QuestionSchema = new Schema({
         default:"Give Question A Title",
         required:true
     },
+    snippet: {
+        type:String
+    },
     questionDescription:{
         required:true,
         type:String,
         default:"No Question Specified"
     },
-    votes:Number,
+    tags:{
+        type:[String],
+        default:"No Tags Given"
+    },
+    upVotes:[{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }],
+    downVotes:[{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }],
+    votes:{
+        type:Number,
+        default:0
+    },
     user:{
         type:Schema.Types.ObjectId,
         ref:"User"
@@ -22,5 +41,13 @@ const QuestionSchema = new Schema({
         ref:"Answer"
     }]
 },{timestamps:true})
+
+//Before saving the questionDescription strip the html tags and save the questionDescription in snippet
+QuestionSchema.pre("save",function(next) {
+    if(this.questionDescription) {
+        this.snippet = stripHTML.stripHtml(this.questionDescription.substring(0,430)).result
+    }
+    next();
+})
 
 module.exports = mongoose.model("Question",QuestionSchema);
