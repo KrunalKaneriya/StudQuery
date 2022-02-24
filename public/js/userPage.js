bf1267c9ba09565d2a7943d48c11db0eaf4d9d25
Krunal Kaneria <prokillerkk@outlook.com>
2022-02-24 23:32:26 +0530

    Disable When Edit Button Not Clicked on User Profile Page

--- public/js/userPage.js
+++ public/js/userPage.js
@@ -1,28 +1,26 @@
 $(document).ready(function () {
-    $("#edit-button-wrapper").click(function() {
-        $("#edit-button").fadeOut("slow");
-        $("#delete-button-wrapper").fadeOut("slow");
-        $("#username").prop("disabled",false);
-        $("#alias").prop("disabled",false);
-        $("#email").prop("disabled",false);
-        $("#age").prop("disabled",false);
-        $("#city").prop("disabled",false);
-        $("#studyingin").prop("disabled",false);
-        $("#description").prop("disabled",false);
-    })
-
-
-})
+	$("#edit-button-wrapper").click(function () {
+		$("#edit-button").fadeOut("slow");
+		$("#delete-button-wrapper").fadeOut("slow");
+		$("#username").prop("disabled", false);
+		$("#alias").prop("disabled", false);
+		$("#email").prop("disabled", false);
+		$("#age").prop("disabled", false);
+		$("#city").prop("disabled", false);
+		$("#studyingin").prop("disabled", false);
+		$("#description").prop("disabled", false);
+	});
+});
 
 let modal = document.getElementById("my-modal");
 let openBtn = document.getElementById("open-btn");
 let deleteBtn = document.getElementById("delete-btn");
 let cancelBtn = document.getElementById("cancel-btn");
 
-openBtn.addEventListener("click",function () {
-    modal.classList.remove("hidden");
-})
+openBtn.addEventListener("click", function () {
+	modal.classList.remove("hidden");
+});
 
-cancelBtn.addEventListener("click",function () {
-    modal.classList.add("hidden");
-})
+cancelBtn.addEventListener("click", function () {
+	modal.classList.add("hidden");
+});
