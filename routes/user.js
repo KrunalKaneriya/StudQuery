a47cd289710848b5200ebed2ff05ea5a8c351637
Krunal Kaneria <prokillerkk@outlook.com>
2022-02-24 23:36:47 +0530

    Added New Features and Error Catching in User route

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,19 @@
+const crypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const Question = require("../models/question");
 const session = require("express-session");
-const crypt = require("bcrypt");
+const Post = require("../models/postSchema");
 const Answer = require("../models/answer");
-const question = require("../models/question");
-
+const catchAsync = require("../utils/catchAsync");
+const ExpressError = require("../utils/ExpressError");
+const answer = require("../models/answer");
 
 /***************************************************
     //TODO CREATING A PAGE WHICH LIST ALL THE USERS *
     //TODO THE ROUTE WILL BE GET /USERS       *
  ***************************************************/
-
 
 /*******************************************************************************************
    //TODO WHEN A LOGGED IN USER CLICKS ON A USER THEN ALLOW THEM TO SEE THE USER DETAILS  *
@@ -19,46 +21,60 @@ const question = require("../models/question");
   //TODO IF THE USER IS THE USER WHICH IS LOGGED IN THEN ALLOW THEM TO EDIT THE DETAILS.  *
  *******************************************************************************************/
 
-router.get("/user/:id",async (req,res) => {
+router.get("/user/:id",catchAsync(async (req, res) => {
+		const userSession = req.session;
 
-    const userSession = req.session;
+		const { id } = req.params;
+		const user = await User.findById(id);
+		const questions = await User.findById(id).populate("questions");
+		const questionCount = await User.findById(id).populate("questions").countDocuments();
+		const answerCount = await User.findById(id).populate("answers").countDocuments();
+		res.render("user", { user, questions, userSession, questionCount, answerCount });
+	})
+);
 
-    const {id} = req.params;
-    const user = await User.findById(id);
-    const questions = await User.findById(id).populate("questions");
-    const questionCount = await User.findById(id).populate("questions").count();
-    const answerCount = await User.findById(id).populate("answers").count();
-    res.render("user",{user,questions,userSession,questionCount,answerCount});
+router.post("/user/:id",catchAsync(async (req, res) => {
+		const { id } = req.params;
+		const user = User.findById(id);
+	})
+);
+
+router.put("/user/:id",catchAsync(async (req, res) => {
+		const { id } = req.params;
+		const user = await User.findByIdAndUpdate(id, { ...req.body.user });
+		res.redirect(`/user/${id}`);
+	})
+);
+
+router.delete(
+	"/user/:id",
+	catchAsync(async (req, res) => {
+		const { id } = req.params;
+		const userSession = req.session;
+		const user = await User.findById(id).populate("questions").populate("answers");
+		const deletedQuestions = await question.deleteMany({ _id: { $in: user.questions } });
+		const deletedUser = await User.findByIdAndDelete(id);
+		const deletedAnswers = await Answer.deleteMany({ _id: { $in: user.answers } });
+		res.redirect("/logout");
+	})
+);
+
+
+router.get("/user/personal/questions",async (req,res) => {
+	
+	const userSession = req.session;
+	
+	
+	if(!userSession.isLoggedIn) {
+		req.flash("error","Log in first to view the Questions.");
+		const redirectUrl = req.session.redirectUrl || '/';
+		res.redirect(redirectUrl);
+	} else {
+		const user = await User.findById(userSession.userid);
+		const result =await User.findById(userSession.userid).populate("questions");
+		res.render("myQuestions",{userSession,result:result.questions,user});
+	}
 })
 
-router.post("/user/:id",async(req,res) => {
-    const {id} = req.params;
-    const user = User.findById(id);
-})
-
-router.put("/user/:id",async(req,res) => {
-    const {id} = req.params;
-    const user = await User.findByIdAndUpdate(id,{ ...req.body.user });
-    res.redirect(`/user/${id}`);
-})
-
-
-router.delete("/user/:id",async (req,res) => {
-    const {id} = req.params;
-    const userSession = req.session;
-    const user = await User.findById(id).populate("questions").populate("answers");
-    const deletedQuestions =await question.deleteMany({_id: {$in : user.questions }});
-    const deletedUser = await User.findByIdAndDelete(id);
-    const deletedAnswers = await Answer.deleteMany({_id : {$in : user.answers}});
-    res.redirect("/logout");
-
-
-})
-
-
-// router.get("/users",async(req,res) => {
-//     const users = await User.find().populate({path:"questions"});
-//     res.json(users);
-// })
 
 module.exports = router;
