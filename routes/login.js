ff14e22d10e7d19df8319dbe31ee002cd0017e78
Krunal Kaneria <prokillerkk@outlook.com>
2022-02-24 23:35:15 +0530

    Edited Login Routes

--- routes/login.js
+++ routes/login.js
@@ -1,60 +1,64 @@
+const crypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const session = require("express-session");
-const crypt = require("bcrypt");
+const Admin = require("../models/admin");
+const catchAsync = require("../utils/catchAsync");
+const ExpressError = require("../utils/ExpressError");
+
+//TODO: Separate the Login Route of Admin and create it in admin route folder
+
+/*******************
+ * VALIDATION SCHEMA *
+ *******************/
+const { logInValidator } = require("../models/validationSchema");
 
 
+/***************************************************************************************************
+ * MAKE A MIDDLEWARE FUNCTION TO GET THE DETAILS OF THE USER AND SEND NEXT OTHERWISE THROW ERROR *
+ ***************************************************************************************************/
 
-router.get("/login",(req,res) => {
-    res.render("login");
-})
+const requestLogin = catchAsync(async (req, res, next) => {
+	const { username, password } = req.body; //Fetching Username And Password From Browser
+	
+	const user = await User.findOne({ $or: [{ username }, { email: username }] }); //Finding The Username Or Email in DB
+	if (user) {
+		//Checking if the User is founded
+		const result = await crypt.compare(password, user.userHash); //Decrypt The password from the db of user
+		const { _id: id } = user; //Take the id from the user founded in the db
 
-router.post("/login",async (req,res) => {
-    const {username,password} = req.body; //Fetching Username And Password From Browser
-    const user = await User.findOne({username}); //Finding The Username in DB
+		//Find The Logged in User and run Update Query to Update the lastLoggedIn To Latest Time
+		await User.findByIdAndUpdate(id, { $set: { lastLoggedIn: new Date() } });
 
-    if(user) { //Checking if the User is founded
+		req.session.username = user.username; //Create a session and add username to it.
+		req.session.userid = user._id; //Add user id into the session
+		req.session.isLoggedIn = true; //Add isLogged in to the session
 
-        const result = await crypt.compare(password,user.userHash); //Decrypt The password from the db of user
-        const {_id : id} = user; //Take the id from the user founded in the db
+		if (result) {
+			return next();
+		}
+	}
+	throw new ExpressError(404, "Username Or Password is Incorrect!");
+});
 
-        //Find The Logged in User and run Update Query to Update the lastLoggedIn To Latest Time
-        await User.findByIdAndUpdate(id , {$set: {"lastLoggedIn" : new Date() }});
+router.get("/login", (req, res) => {
+	res.render("login");
+});
 
-        req.session.username = user.username; //Create a session and add username to it.
-        req.session.userid = user._id; //Add user id into the session
-        req.session.isLoggedIn =  true; //Add isLogged in to the session
-        
-        if(result) {
-            res.redirect("/");
-        } else {
-            res.send("Username Or Password is Incorrect");
-        }
-    } else {
-        res.send("User is not Registered");
-    }
-    
-    
-    
-})
+router.post("/login",requestLogin,logInValidator,catchAsync(async (req, res) => {
+		req.flash("welcome", "Welcome to Studquery");
+		const redirectUrl = req.session.redirectUrl || '/';
+		res.redirect(redirectUrl);
+	})
+);
 
-router.get("/logout",(req,res) => {
-    if(req.session.isLoggedIn) {
-        req.session.destroy(() => {
-            res.redirect("/");
-        });
-    }
-})
+
+router.get("/logout", (req, res) => {
+	if (req.session.isLoggedIn) {
+		req.session.destroy(() => {
+			res.redirect("/");
+		});
+	}
+});
 
 module.exports = router;
-
-
-/*
-1.To Login Using Username and Password
-2.To check that the user is there 
-3.and if the user is there then get the userhash and check the password and if it is valid then allow the user to login
-4.Otherwise send that the user is not found
-
-
-*/
